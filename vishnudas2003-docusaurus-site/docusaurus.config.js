633af242de943eaa2f3df2347c53671d1f1ddaa0
vishnudas <vishnudas7412@gmail.com>
2023-10-29 19:59:37 -0400

    Updated config.js

--- vishnudas2003-docusaurus-site/docusaurus.config.js
+++ vishnudas2003-docusaurus-site/docusaurus.config.js
@@ -18,9 +18,9 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
+  organizationName: 'Vishnudas2003', // Usually your GitHub org/user name.
   projectName: 'docusaurus', // Usually your repo name.
-
+  deploymentBranch: 'main', // The branch of your site that GitHub pages will deploy from.
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
