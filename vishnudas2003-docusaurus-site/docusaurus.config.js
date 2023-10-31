ed4909247fbe23a529be41185f812fcd23f69823
vishnudas <vishnudas7412@gmail.com>
2023-10-31 12:05:12 -0400

    Updated config.js

--- vishnudas2003-docusaurus-site/docusaurus.config.js
+++ vishnudas2003-docusaurus-site/docusaurus.config.js
@@ -13,7 +13,7 @@ const config = {
   url: 'https://Vishnudas2003.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/vishnudas2003-docusaurus-site/vishnudas2003-docusaurus-site/',
+  baseUrl: '/vishnudas2003-docusaurus-site/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
